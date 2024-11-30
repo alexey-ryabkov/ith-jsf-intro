14d5d7ac75c16446ded0171ac18578938054bfb0
alexey-ryabkov <xelaris@ya.ru>
2024-11-30 20:46:45 +0300

    chore: del starter app

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from '@containers/App';
-// import reportWebVitals from './reportWebVitals';
 import '@styles/global.scss';
 
 const container = document.getElementById('root')!;
@@ -11,10 +8,6 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
-
-// reportWebVitals();
